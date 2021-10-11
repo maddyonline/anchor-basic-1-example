96ccaf6b5eac892cded1f1bfbba6c1ac7dcca7b5
Madhav Jha <madhavjha@gmail.com>
2021-10-11 13:30:54 -0700

    create a check

--- tests/xbasic-1.js
+++ tests/xbasic-1.js
@@ -1,5 +1,7 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
+const serumCmn = require("@project-serum/common");
+const { TOKEN_PROGRAM_ID } = require("@solana/spl-token");
 const { SystemProgram } = anchor.web3;
 
 describe("basic-1", () => {
@@ -8,6 +10,62 @@ describe("basic-1", () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(provider);
+
+  let mint = null;
+  let god = null;
+  let receiver = null;
+
+  it("Sets up initial test state", async () => {
+    const program = anchor.workspace.Xbasic1;
+    const [_mint, _god] = await serumCmn.createMintAndVault(
+      program.provider,
+      new anchor.BN(1_000_000)
+    );
+    mint = _mint;
+    god = _god;
+
+    receiver = await serumCmn.createTokenAccount(
+      program.provider,
+      mint,
+      program.provider.wallet.publicKey
+    );
+  });
+  const check = anchor.web3.Keypair.generate();
+  const vault = anchor.web3.Keypair.generate();
+
+  let checkSigner = null;
+
+  it("Creates a check!", async () => {
+    const program = anchor.workspace.Xbasic1;
+    let [_checkSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [check.publicKey.toBuffer()],
+      program.programId
+    );
+    checkSigner = _checkSigner;
+
+    await program.rpc.createCheck(new anchor.BN(100), "Hello world", nonce, {
+      accounts: {
+        check: check.publicKey,
+        vault: vault.publicKey,
+        checkSigner,
+        from: god,
+        to: receiver,
+        owner: program.provider.wallet.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [check, vault],
+      instructions: [
+        await program.account.check.createInstruction(check, 300),
+        ...(await serumCmn.createTokenAccountInstrs(
+          program.provider,
+          vault.publicKey,
+          mint,
+          checkSigner
+        )),
+      ],
+    });
+  });
 
   it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
     // #region code-simplified
